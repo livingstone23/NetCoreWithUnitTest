0431ee318417c19ee8338ea45ff763572ccb7e39
livingstone23 <livingstone23@gmail.com>
2022-02-19 16:48:43 +0100

    Bongo core separating Unit Tests

--- Bongo.Core.Tests/StudyRoomBookingServiceTests.cs
+++ Bongo.Core.Tests/StudyRoomBookingServiceTests.cs
@@ -5,6 +5,8 @@ using System.Text;
 using System.Threading.Tasks;
 using Bongo.Core.Services;
 using Bongo.DataAccess.Repository.IRepository;
+using Bongo.Models.Model;
+using Bongo.Models.Model.VM;
 using Moq;
 using NUnit.Framework;
 
@@ -13,6 +15,8 @@ namespace Bongo.Core
     [TestFixture]
     public class StudyRoomBookingServiceTests
     {
+        private StudyRoomBooking _request;
+        private List<StudyRoom> _availableStudyRoom;
 
         private Mock<IStudyRoomBookingRepository> _studyRoomBookingRepoMock;
         private Mock<IStudyRoomRepository> _studyRoomRepoMock;
@@ -21,8 +25,24 @@ namespace Bongo.Core
         [SetUp]
         public void Setup()
         {
+            _request = new StudyRoomBooking
+            {
+                FirstName = "Ben",
+                LastName = "Spark",
+                Email = "ben@gmail.com",
+                Date = new DateTime(2022, 1, 1)
+            };
+
+            _availableStudyRoom = new List<StudyRoom> {
+                new StudyRoom{
+                    Id=10,RoomName="Michigan", RoomNumber="A202"
+                }
+            };
+
             _studyRoomBookingRepoMock = new Mock<IStudyRoomBookingRepository>();
             _studyRoomRepoMock = new Mock<IStudyRoomRepository>();
+
+            _studyRoomRepoMock.Setup(x => x.GetAll()).Returns(_availableStudyRoom);
 
             _bookingService = new StudyRoomBookingService(
                 _studyRoomBookingRepoMock.Object,
@@ -30,6 +50,7 @@ namespace Bongo.Core
             );
 
         }
+
 
 
         [TestCase]
@@ -41,6 +62,100 @@ namespace Bongo.Core
             //Confirm that execute one.
         }
 
+
+
+        [TestCase]
+        public void BookingException_NullRequest_ThrowsException()
+        {
+            var exception = Assert.Throws<ArgumentNullException>(() => _bookingService.BookStudyRoom(null));
+            //Assert.AreEqual("Value cannot be null. (Parameter 'request')", exception.Message);    
+            Assert.AreEqual("request", exception.ParamName);
+        }
+
+
+
+        [Test]
+        public void StudyRoomBooking_SaveBookingWithAvailableRoom_ReturnsResultWithAllValues()
+        {
+            StudyRoomBooking savedStudyRoomBooking = null;
+            _studyRoomBookingRepoMock.Setup(x => x.Book(It.IsAny<StudyRoomBooking>()))
+                .Callback<StudyRoomBooking>(booking =>
+                {
+                    savedStudyRoomBooking = booking;
+                });
+
+            //act
+            _bookingService.BookStudyRoom(_request);
+
+            //assert
+            _studyRoomBookingRepoMock.Verify(x => x.Book(It.IsAny<StudyRoomBooking>()), Times.Once);
+
+            Assert.NotNull(savedStudyRoomBooking);
+            Assert.AreEqual(_request.FirstName, savedStudyRoomBooking.FirstName);
+            Assert.AreEqual(_request.LastName, savedStudyRoomBooking.LastName);
+            Assert.AreEqual(_request.Email, savedStudyRoomBooking.Email);
+            Assert.AreEqual(_request.Date, savedStudyRoomBooking.Date);
+            Assert.AreEqual(_availableStudyRoom.First().Id, savedStudyRoomBooking.StudyRoomId);
+        }
+
+
+
+        [Test]
+        public void StudyRoomBookingResultCheck_InputRequest_ValuesMatchInResult()
+        {
+            StudyRoomBookingResult result = _bookingService.BookStudyRoom(_request);
+
+            Assert.NotNull(result);
+            Assert.AreEqual(_request.FirstName, result.FirstName);
+            Assert.AreEqual(_request.LastName, result.LastName);
+            Assert.AreEqual(_request.Email, result.Email);
+            Assert.AreEqual(_request.Date, result.Date);
+        }
+
+
+
+        [Test]
+        public void ResultCodeSuccess_RoomAvability_ReturnsSuccessResultCode()
+        {
+            var result = _bookingService.BookStudyRoom(_request);
+
+            Assert.AreEqual(StudyRoomBookingCode.Success, result.Code);
+        }
+
+
+
+        [TestCase(true, ExpectedResult = StudyRoomBookingCode.Success)]
+        [TestCase(false, ExpectedResult = StudyRoomBookingCode.NoRoomAvailable)]
+        public StudyRoomBookingCode ResultCodeSuccess_RoomAvability_ReturnsSuccessResultCode(bool roomAvailability)
+        {
+            if (!roomAvailability)
+            {
+                _availableStudyRoom.Clear();
+            }
+            return _bookingService.BookStudyRoom(_request).Code;
+        }
+
+
+
+        [TestCase(0, false)]
+        [TestCase(55, true)]
+        public void StudyRoomBooking_BookRoomWithAvailalbility_ReturnsBookingId(int expectedBookingId, bool roomAvailability)
+        {
+            if (!roomAvailability)
+            {
+                _availableStudyRoom.Clear();
+            }
+
+
+            _studyRoomBookingRepoMock.Setup(x => x.Book(It.IsAny<StudyRoomBooking>()))
+                .Callback<StudyRoomBooking>(booking =>
+                {
+                    booking.BookingId = 55;
+                });
+
+            var result = _bookingService.BookStudyRoom(_request);
+            Assert.AreEqual(expectedBookingId, result.BookingId);
+        }
 
 
 
