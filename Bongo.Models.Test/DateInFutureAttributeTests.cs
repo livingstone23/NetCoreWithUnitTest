fe1329b8d7d9d2135f4fec7886e1e1edd7f80ca7
livingstone23 <livingstone23@gmail.com>
2022-02-19 11:54:42 +0100

    Testing Models, DateChecker

---
+++ Bongo.Models.Test/DateInFutureAttributeTests.cs
@@ -0,0 +1,49 @@
+﻿using Bongo.Models.ModelValidations;
+using NUnit.Framework;
+
+namespace Bongo.Models
+{
+    [TestFixture]
+    public class DateInFutureAttributeTests
+    {
+
+        
+
+        [Test]
+        public void DateValidator_InputExpectedDateRange_DateValidity_Test()
+        {
+
+            DateInFutureAttribute dateInFutureAttribute = new(()=> DateTime.Now);
+            var result = dateInFutureAttribute.IsValid(DateTime.Now.AddSeconds(100));
+            Assert.AreEqual(true, result);
+
+
+        }
+
+
+
+        [TestCase(100, ExpectedResult = true)]
+        [TestCase(-100, ExpectedResult = false)]
+        [TestCase(0, ExpectedResult = false)]
+        public bool DateValidator_InputExpectedDateRange_DateValidity_TestCase(int addTime)
+        {
+
+            DateInFutureAttribute dateInFutureAttribute = new(() => DateTime.Now);
+            return dateInFutureAttribute.IsValid(DateTime.Now.AddSeconds(addTime));
+
+        }
+
+
+
+        [Test]
+        public void DateValidator_NotValidDate_ReturnErrorMessage()
+        {
+
+            var result = new DateInFutureAttribute();
+            Assert.AreEqual("Date must be in the future", result.ErrorMessage);
+
+        }
+
+    }
+
+}
