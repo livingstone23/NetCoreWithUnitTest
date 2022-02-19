10ce0c00f95b186c9f05f081b612de4191baf020
livingstone23 <livingstone23@gmail.com>
2022-02-19 12:52:01 +0100

    Integrating Data Access Unit Test

---
+++ Bongo.DataAccess.Tests/StudyRoomBookingRepositoryTests.cs
@@ -0,0 +1,144 @@
+﻿using Bongo.DataAccess.Repository;
+using Bongo.Models.Model;
+using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using System.Collections;
+
+namespace Bongo.DataAccess
+{
+    [TestFixture]
+    public class StudyRoomBookingRepositoryTests
+    {
+
+        private StudyRoomBooking studyRoomBooking_One;
+        private StudyRoomBooking studyRoomBooking_Two;
+        //Variable para el manejo del contexto a inicializar en el SetUp
+        private DbContextOptions<ApplicationDbContext> options;
+
+
+        public StudyRoomBookingRepositoryTests()
+        {
+            studyRoomBooking_One = new StudyRoomBooking()
+            {
+                FirstName = "Livingstone",
+                LastName = "Cano",
+                Date = new DateTime(2023, 1, 1),
+                Email = "test@gmail.coom",
+                BookingId = 11,
+                StudyRoomId = 1
+            };
+
+            studyRoomBooking_Two = new StudyRoomBooking
+            {
+                FirstName = "Mary",
+                LastName = "Hernandez",
+                Date = new DateTime(2023, 2, 2),
+                Email = "test2@gmail.coom",
+                BookingId = 22,
+                StudyRoomId = 2
+            };
+        }
+
+
+        //Inicializamos el contexto
+        [SetUp]
+        public void Setup()
+        {
+            options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: "temp_Bongo").Options;
+        }
+
+        [Test]
+        [Order(1)]
+        public void SaveBooking_Booking_One_CheckTheValuesFromDatabase()
+        {
+
+            //Arrange
+            //Create in memory Database
+            //var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+            //    .UseInMemoryDatabase(databaseName: "temp_Bongo").Options;
+            //"Contexto removido al Setup"
+
+            //Act
+            using (var context = new ApplicationDbContext(options))
+            {
+                var repository = new StudyRoomBookingRepository(context);
+                repository.Book(studyRoomBooking_One);
+            }
+
+            //Assert
+            using (var context = new ApplicationDbContext(options))
+            {
+                var bookingFromDb = context.StudyRoomBookings.FirstOrDefault(u => u.BookingId == 11);
+                Assert.AreEqual(studyRoomBooking_One.BookingId, bookingFromDb.BookingId);
+                Assert.AreEqual(studyRoomBooking_One.FirstName, bookingFromDb.FirstName);
+                Assert.AreEqual(studyRoomBooking_One.LastName, bookingFromDb.LastName);
+                Assert.AreEqual(studyRoomBooking_One.Email, bookingFromDb.Email);
+                Assert.AreEqual(studyRoomBooking_One.Date, bookingFromDb.Date);
+            }
+
+        }
+
+
+
+        [Test]
+        [Order(2)]
+        public void GetAllBooking_BookingOneAndTwo_CheckBoththeBookingFromDatabase()
+        {
+
+            //Arrange
+            //Expecting result
+            var expectedResult = new List<StudyRoomBooking> { studyRoomBooking_One, studyRoomBooking_Two };
+            //var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+            //    .UseInMemoryDatabase(databaseName: "temp_Bongo").Options;
+            //"Contexto removido al Setup"
+
+
+            using (var context = new ApplicationDbContext(options))
+            {
+                //Permite limpiar el contexto
+                context.Database.EnsureDeleted();
+                var repository = new StudyRoomBookingRepository(context);
+                repository.Book(studyRoomBooking_One);
+                repository.Book(studyRoomBooking_Two);
+            }
+
+            //Act
+            List<StudyRoomBooking> actualList;
+            using (var context = new ApplicationDbContext(options))
+            {
+                var repository = new StudyRoomBookingRepository(context);
+                actualList = repository.GetAll(null).ToList();
+
+            }
+
+
+            //Assert
+            //Compare the two list
+            CollectionAssert.AreEqual(expectedResult, actualList, new BookingCompare());
+
+        }
+
+
+        private class BookingCompare : IComparer
+        {
+            public int Compare(object x, object y)
+            {
+                var booking1 = (StudyRoomBooking)x;
+                var booking2 = (StudyRoomBooking)y;
+                if (booking1.BookingId != booking2.BookingId)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }
+
+
+
+
+    }
+}
